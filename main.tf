28c0295cfcd4f6564680c7709734f201de29c70f
Devron Tombacco <tombacco.devron@gmail.com>
2025-09-02 10:14:50 +0100

    create 2 private subnets, with hardcoded ip addresses (will refactor later)

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@
 resource "aws_vpc" "main_vpc" {
   cidr_block = "10.0.0.0/16"
 
-   tags = {
+  tags = {
     Name = "main_vpc"
   }
 }
@@ -14,5 +14,27 @@ resource "aws_internet_gateway" "igw" {
 
   tags = {
     Name = "igw"
+  }
+}
+
+# Create 1st public subnet
+resource "aws_subnet" "public_subnet1a" {
+  vpc_id                  = aws_vpc.main_vpc.id
+  cidr_block              = var.public_subnet1a_cidr
+  availability_zone       = var.availability_zone_1a
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "public_subnet1a"
+  }
+}
+
+# Create 2nd public subnet
+resource "aws_subnet" "public_subnet1b" {
+  vpc_id                  = aws_vpc.main_vpc.id
+  cidr_block              = var.public_subnet1b_cidr
+  availability_zone       = var.availability_zone_1b
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "public_subnet1b"
   }
 }
