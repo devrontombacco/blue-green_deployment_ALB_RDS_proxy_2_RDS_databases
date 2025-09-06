22ea53933cd93bc59b5e14874ab2070864882a5c
Devron Tombacco <tombacco.devron@gmail.com>
2025-09-06 16:49:59 +0100

    Add public route table

--- main.tf
+++ main.tf
@@ -60,3 +60,19 @@ resource "aws_subnet" "private_subnet1d_nat_env" {
     Name = "public_subnet1d_nat_env"
   }
 }
+
+# Create Public Route Table
+
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "public_rt"
+  }
+
+}
