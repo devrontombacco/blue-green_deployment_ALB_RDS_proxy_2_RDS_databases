5989d7e3987b34203d2081880f8b8ac780de5656
Devron Tombacco <tombacco.devron@gmail.com>
2025-09-08 19:30:27 +0100

    add SG for ALB

--- main.tf
+++ main.tf
@@ -169,3 +169,30 @@ resource "aws_security_group" "sg_bastion_host" {
   }
 
 }
+
+# Create Security Group for ALB
+resource "aws_security_group" "sg_alb" {
+  name        = "sg_alb"
+  description = "Allow inbound http traffic"
+  vpc_id      = aws_vpc.main_vpc.id
+
+  tags = {
+    Name = "sg_alb"
+  }
+
+  ingress {
+    description = "http from anywhere"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+}
