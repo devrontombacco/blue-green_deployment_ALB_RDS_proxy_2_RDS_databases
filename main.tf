a31b836a72922d50f0acc196061d9674235c541d
Devron Tombacco <tombacco.devron@gmail.com>
2025-09-08 19:20:48 +0100

    Create fallback ip address for my_ip_address

--- main.tf
+++ main.tf
@@ -137,6 +137,13 @@ resource "aws_route_table_association" "private_1b" {
   route_table_id = aws_route_table.private_rt.id
 }
 
+# Create fallback var for my_ip_address 
+variable "my_ip_address" {
+  type    = string
+  default = "0.0.0.0/0" # fallback IP
+}
+
+
 # Create Security Group for Bastion Host 
 resource "aws_security_group" "sg_bastion_host" {
   name        = "sg_bastion_host"
