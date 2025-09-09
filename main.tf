d6065c117b6e3e888d6c127a730bf11ed5337599
Devron Tombacco <tombacco.devron@gmail.com>
2025-09-09 07:00:58 +0100

    Create AMI data source

--- main.tf
+++ main.tf
@@ -223,3 +223,30 @@ resource "aws_security_group" "sg_ec2_private" {
   }
 
 }
+
+# Create AMI data source
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"]
+}
+
+resource "aws_instance" "example" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "ami_for_ec2"
+  }
+}
+
