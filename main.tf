7f947d64ed3cce2c3426c0fa6511a360313d26c3
Devron Tombacco <tombacco.devron@gmail.com>
2025-09-09 06:36:10 +0100

    create SG for private ec2 instances

--- main.tf
+++ main.tf
@@ -196,3 +196,30 @@ resource "aws_security_group" "sg_alb" {
   }
 
 }
+
+# Create Security Group for private EC2 instances
+resource "aws_security_group" "sg_ec2_private" {
+  name        = "sg_ec2_private"
+  description = "Allow inbound http traffic from ALB"
+  vpc_id      = aws_vpc.main_vpc.id
+
+  tags = {
+    Name = "sg_ec2_private"
+  }
+
+  ingress {
+    description     = "http from ALB"
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = [aws_security_group.sg_alb.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+}
