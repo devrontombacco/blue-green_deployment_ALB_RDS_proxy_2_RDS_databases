de43669f8bd728703f373e02935c1d323dd50503
Devron Tombacco <tombacco.devron@gmail.com>
2025-09-17 20:52:47 +0100

    refactor subnet values into variables.tf + terraform.tfvars files

--- network.tf
+++ network.tf
@@ -2,7 +2,7 @@
 
 # Create a VPC
 resource "aws_vpc" "main_vpc" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block = var.vpc_cidr
 
   tags = {
     Name = "main_vpc"
@@ -21,7 +21,7 @@ resource "aws_internet_gateway" "igw" {
 # Create 1st subnet - private
 resource "aws_subnet" "private_subnet1a_blue_env" {
   vpc_id                  = aws_vpc.main_vpc.id
-  cidr_block              = "10.0.1.0/24"
+  cidr_block              = var.private_subnet_1a
   availability_zone       = "eu-west-1a"
   map_public_ip_on_launch = false
   tags = {
@@ -32,7 +32,7 @@ resource "aws_subnet" "private_subnet1a_blue_env" {
 # Create 2nd subnet - private
 resource "aws_subnet" "private_subnet1b_green_env" {
   vpc_id                  = aws_vpc.main_vpc.id
-  cidr_block              = "10.0.2.0/24"
+  cidr_block              = var.private_subnet_1b
   availability_zone       = "eu-west-1b"
   map_public_ip_on_launch = false
   tags = {
@@ -43,7 +43,7 @@ resource "aws_subnet" "private_subnet1b_green_env" {
 # Create 3rd subnet - public
 resource "aws_subnet" "public_subnet1c_admin_env" {
   vpc_id                  = aws_vpc.main_vpc.id
-  cidr_block              = "10.0.3.0/24"
+  cidr_block              = var.admin_subnet_1c
   availability_zone       = "eu-west-1c"
   map_public_ip_on_launch = false
   tags = {
@@ -54,7 +54,7 @@ resource "aws_subnet" "public_subnet1c_admin_env" {
 # Create 4th subnet - public
 resource "aws_subnet" "public_subnet1d_nat_env" {
   vpc_id                  = aws_vpc.main_vpc.id
-  cidr_block              = "10.0.4.0/24"
+  cidr_block              = var.public_subnet_1d
   availability_zone       = "eu-west-1c"
   map_public_ip_on_launch = false
   tags = {
@@ -65,7 +65,7 @@ resource "aws_subnet" "public_subnet1d_nat_env" {
 # Create 5th subnet - public
 resource "aws_subnet" "public_subnet1e_nat_env" {
   vpc_id                  = aws_vpc.main_vpc.id
-  cidr_block              = "10.0.5.0/24"
+  cidr_block              = var.public_subnet_1e
   availability_zone       = "eu-west-1b"
   map_public_ip_on_launch = false
   tags = {
--- variables.tf
+++ variables.tf
@@ -13,3 +13,27 @@ variable "key_name" {
   type        = string
   description = "key name for ec2"
 }
+
+variable "private_subnet_1a" {
+  type        = string
+  description = "cider block for first privae subnet "
+}
+
+variable "private_subnet_1b" {
+  type        = string
+  description = "cider block for second private  subnet "
+}
+variable "admin_subnet_1c" {
+  type        = string
+  description = "cider block for admin subnet"
+}
+
+variable "public_subnet_1d" {
+  type        = string
+  description = "cider block for public subnet d "
+}
+
+variable "public_subnet_1e" {
+  type        = string
+  description = "cider block for piblic subnet e"
+}
